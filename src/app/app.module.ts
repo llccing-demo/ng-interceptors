ea02acd57fec6bb41b2f875be40542c129ebdf62
liuchunfeng <chunfeng.liu@innocellence.com>
2020-12-31 11:25:37 +0800

    feat: update loader interceptor logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,28 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { httpInterceptorProviders } from './interceptors'
+import { httpInterceptorProviders } from './interceptors';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+registerLocaleData(zh);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [httpInterceptorProviders],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        NzButtonModule
+    ],
+    providers: [httpInterceptorProviders, { provide: NZ_I18N, useValue: zh_CN }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -1,7 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HttpsInterceptor } from './https.interceptor';
+import { LoaderInterceptor } from './loader.interceptor';
 
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true },
+    // export const httpInterceptorProviders: [{ provide: typeof HTTP_INTERCEPTORS; useClass: any; multi: any }] = [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
 ];
