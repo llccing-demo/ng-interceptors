e5c00cc70f35d7980dc7d00147d8c9cf06931fe5
liuchunfeng <chunfeng.liu@innocellence.com>
2020-12-31 15:18:06 +0800

    feat: setHeaders X-man

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HttpsComponent } from './pages/https/https.component';
 import { LoaderComponent } from './pages/loader/loader.component';
 import { ConvertComponent } from './pages/convert/convert.component';
+import { HeadersComponent } from './pages/headers/headers.component';
 
 const routes: Routes = [
     {
@@ -23,11 +24,15 @@ const routes: Routes = [
     {
         path: 'convert',
         component: ConvertComponent
+    },
+    {
+        path: 'headers',
+        component: HeadersComponent
     }
 ];
 
 @NgModule({
-    declarations: [HttpsComponent, LoaderComponent, ConvertComponent],
+    declarations: [HttpsComponent, LoaderComponent, ConvertComponent, HeadersComponent],
     imports: [CommonModule, RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -3,10 +3,12 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpsInterceptor } from './https.interceptor';
 import { LoaderInterceptor } from './loader.interceptor';
 import { ConvertInterceptor } from './convert.interceptor';
+import { HeadersInterceptor } from './headers.interceptor';
 
 export const httpInterceptorProviders = [
     // export const httpInterceptorProviders: [{ provide: typeof HTTP_INTERCEPTORS; useClass: any; multi: any }] = [
     { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ConvertInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ConvertInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true }
 ];
